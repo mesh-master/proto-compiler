548b0b5331556578fe70bc7ce0a164f85872b52c
Andrei Samuilik <a.samuylik@gmail.com>
2022-03-14 17:59:12 +0300

    Install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
 
 # Golang linter
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.44.2
+if ! which golangci-lint &>/dev/null; then
+  echo "Installing golangci-lint..."
+  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh \
+  | sh -s -- -b $(go env GOPATH)/bin v1.44.2
+else
+  echo "golangci-lint has been found, skipping installation"
+fi
