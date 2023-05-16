616e3a64ca5aa7193ae91cf22512346dfdb2f3e3
user <a.samuylik@gmail.com>
2023-05-16 18:01:15 +0300

    misc

--- Dockerfile
+++ Dockerfile
@@ -12,11 +12,6 @@ ENV GOBIN=/usr/bin
 ENV TERM=xterm-color
 ENV PS1='\e[33;1m\u@goenv-\h: \e[31m\W\e[0m\$ '
 
-# Install utilities
-ADD . /app
-RUN set -e \
-    && cd /app && go install ./...
-
 # Install protoc
 RUN set -e \
     && apt update \
@@ -29,7 +24,8 @@ RUN set -e \
     &&  cd protobuf \
     &&  git submodule update --init --recursive \
     &&  bazel build :protoc :protobuf \
-    &&  cp bazel-bin/protoc /usr/bin/
+    &&  cp bazel-bin/protoc /usr/bin/ \
+    &&  cp -r src/google /usr/include/
 
 # Install protoc-gen-go
 RUN set -e \
@@ -39,13 +35,18 @@ RUN set -e \
     && cd /tmp/grpc-go/cmd/protoc-gen-go-grpc \
     && go install ./...
 
-#ENV GOROOT=/usr/go
+RUN set -e \
+    && apt update \
+    && apt install -y inotify-tools vim gawk
 
 COPY ./scripts/.profile /root
 RUN chmod +x /root/.profile
 
+# Install utilities
+ADD . /app
+RUN set -e \
+    && cd /app && go install ./...
+
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
-
-SHELL ["/bin/bash", "-ec"]
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,9 @@ NORMAL=$(tput sgr0)
 
 PROTO_COMPILE_DELAY=${PROTO_COMPILE_DELAY:=4000}
 PROTO_OUT_BASE=/app/proto
-GO_TARGET="go"
-CJS_TARGET="commonjs"
 
-_go_basedir=/app/autogen/go
-_cjs_basedir=/app/autogen/cjs
+# This directory should be mounted onto a host one on docker-compose.yml, usually ./internal/autogen
+GO_TARGET_DIR=/app/autogen/go
 
 # Read the target platforms into an array variable.
 # Supported platforms: commonjs, go
@@ -39,13 +37,13 @@ get_go_path_postfix() {
 autogen_go_package_option() {
   local file=$1
   local go_path_postfix=$(get_go_path_postfix $file)
-  echo -n $(sed -i -r "/^\s*package\s+[^;]+;/a${NL}option go_package = \"$GS_GO_IMPORT_PREFIX/$go_path_postfix\";" $file)
+  echo -n $(sed -i -r "/^\s*package\s+[^;]+;/a${NL}option go_package = \"$PBC_GO_IMPORT_PREFIX/$go_path_postfix\";" $file)
 }
 
 compile_go_proto() {
   local tmstmp=proto-go-$(date +%s)
   local proto_tmp="/tmp/$tmstmp"
-  local proto_out="${_go_basedir}"
+  local proto_out="${GO_TARGET_DIR}"
 
   mkdir -p $proto_tmp
   mkdir -p $proto_out
@@ -78,54 +76,26 @@ compile_go_proto() {
   chown -R $USER_ID:$GROUP_ID $proto_out
 }
 
-compile_cjs_proto() {
-  local tmstmp=proto-cjs-$(date +%s)
-  local proto_tmp="/tmp/$tmstmp"
-  local proto_out="${_cjs_basedir}"
-
-  mkdir -p $proto_tmp
-  mkdir -p $proto_out
-  rm -rf $proto_out/*
-  # Copy proto files to compile
-  cp -r /share/* $proto_tmp/
-
-  started_at "compiling proto files, target CommonJS"
-  find $proto_tmp -name '*.proto' -type f | while read file;
-    do
-      echo -e "\t...$(basename $file)"
-      protoc \
-        -I"$proto_tmp" \
-        --js_out=import_style=commonjs,binary:$proto_out \
-        --ts_out=service=grpc-web:$proto_out \
-        $file
-    done
-  _done
-
-  rm -rf $proto_tmp
-  chown -R $USER_ID:$GROUP_ID $proto_out
-}
-
-compile_all() {
-    if [[ -d "$_go_basedir" ]]; then
+compile() {
+    if [[ -d "$GO_TARGET_DIR" ]]; then
       compile_go_proto
-    fi
-    if [[ -d "$_cjs_basedir" ]]; then
-      compile_cjs_proto
+    else
+      echo "nothing to compile, please specify a target directory in docker-compose.yml"
     fi
 }
 
-#
-#
 #
 source ~/.profile
 
-#
-compile_all
+# Compile at container start.
+compile
 
 # Monitor changes of all proto files and compile them after each change
-inotifywait -m -e close_write -e delete -r --include='.proto$' /share \
-  | debounce -t $PROTO_COMPILE_DELAY --replace-input="$(echo)" \
-  | while read;
+inotifywait -m -e close_write -e delete -r /share \
+  | debounce -t $PROTO_COMPILE_DELAY \
+  | while read -r file;
 do
-  compile_all
+  if [[ $file =~ \.proto$ ]]; then
+    compile
+  fi
 done
