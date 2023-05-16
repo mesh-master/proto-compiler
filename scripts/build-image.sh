b826fd7930d1a88c60d93820a18f8ace2c1c3011
user <a.samuylik@gmail.com>
2023-05-16 13:14:50 +0300

    misc

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -3,8 +3,7 @@
 PROTOBUF_VER=${PROTOBUF_VER:=3.19.4}
 
 docker build \
-  --build-arg PROTOBUF_VER="$PROTOBUF_VER" \
   --build-arg USER_ID=$(id -u) \
   --build-arg GROUP_ID=$(id -g) \
-  -t go-serv_proto-compiler \
+  -t pb-compiler \
   .
